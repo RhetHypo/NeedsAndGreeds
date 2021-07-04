9a650b209a345fece42a9c8e25c281964959a86c
RhetHypo <65476977+RhetHypo@users.noreply.github.com>
2021-07-04 00:14:48 -0400

    Added specialists, fixed pooled calculations

--- Eed.gd
+++ Eed.gd
@@ -3,6 +3,7 @@ extends TextureRect
 export var needs = []
 export var greeds = []
 export var balances = []
+export var money = 0
 #export var volatility = 1
 export var type = "None"
 
@@ -23,53 +24,55 @@ func init(set_need, set_greed, set_type):
 	for i in range(0,balances.size()):
 		balances[i] = 0
 	type = set_type
-	hint_tooltip = "Type: " + type + ", Need: " + str(needs) + ", Greed: " + str(greeds) + ", Current: " + str(balances)
+	hint_tooltip = "Type: " + type + ", Need: " + str(needs) + ", Greed: " + str(greeds) + ", Current: " + str(balances) + ", Money: " + str(money)
 	set_status(STATUS.ALIVE)
 
 func work(volatility = 0):
-	for i in needs.size():
+	for i in range(0,needs.size()):
 		if status != STATUS.DEAD:
 			self.balances[i] = self.balances[i] + self.greeds[i] + rng.randi_range(-volatility, volatility)
-	survive()
+	#survive()
 
 func tax(index, volatility = 0, partial = 0):
-	for i in range(0,needs.size()-1):
-		if status != STATUS.DEAD:
-			self.balances[i] = self.balances[i] + self.greeds[i]
-			if partial != 0:
-				if balances[i] > partial:
-					var temp = partial
-					balances[i] = balances[i] - partial
-					return temp
-				else:
-					var temp = balances[i]
-					balances[i] = 0
-					return temp
-			else:
-				var temp = balances[i]
-				balances[i] = 0
+	if status != STATUS.DEAD:
+		self.balances[index] = self.balances[index] + self.greeds[index] + rng.randi_range(-volatility, volatility)
+		if partial != 0:
+			if balances[index] > partial:
+				var temp = partial
+				balances[index] = balances[index] - partial
 				return temp
-		else:
-			var temp = balances[i]
-			balances[i] = 0
-			return temp
+	var temp = self.balances[index]
+	self.balances[index] = 0
+	return temp
 
 func stim(partial,i):
 	balances[i] = balances[i] + partial
 
-func buy():
-	pass
+func buy(index, price, max_amount):
+	var required_amount = needs[index] - balances[index]
+	var required_price = (required_amount) * price
+	if required_amount > 0 and required_amount <= max_amount and required_price < money:
+		money -= required_price
+		balances[index] += required_amount
+		return required_amount
+	return 0
 
-func sell():
-	pass
+func sell(index, price):
+	var profit = balances[index] - needs[index]
+	if profit > 0:
+		money += profit * price
+		balances[index] -= profit
+		return profit
+	return 0
 
 func survive():
-	for i in range(0,needs.size()-1):
-		balances[i] = balances[i] - needs[i]
-		hint_tooltip = "Type: " + type + ", Need: " + str(needs) + ", Greed: " + str(greeds) + ", Current: " + str(balances)
-		if balances[i] < 0 and self.status != STATUS.DEAD:
-			balances[i] = 0
-			self.set_status(STATUS.DEAD)
+	if self.status != STATUS.DEAD:
+		for i in range(0,needs.size()):
+			balances[i] = balances[i] - needs[i]
+			hint_tooltip = "Type: " + type + ", Need: " + str(needs) + ", Greed: " + str(greeds) + ", Current: " + str(balances)
+			if balances[i] < 0:
+				balances[i] = 0
+				self.set_status(STATUS.DEAD)
 
 func set_status(new_status):
 	status = new_status
--- ZaWarudo.gd
+++ ZaWarudo.gd
@@ -1,16 +1,18 @@
 extends CenterContainer
 
 export var population = 25
-export var turns = 50
-export var volatility = 1
-export var deviation = 10
-export var poor = 10
-export var rich = 10
+export var turns = 5
+export var volatility = 0
+export var deviation = 4
+export var poor = 0
+export var rich = 0
+export var spec = 10
 export var slow = false
-export var realism = true#need is increased to be a percentage of greed, a.k.a. you need to spend money to make money
+export var realism = false#need is increased to be a percentage of greed, a.k.a. you need to spend money to make money
 export var realism_amplifier = .75
 export var need_amplifier = 5
 export var greed_amplifier = 5
+export var spec_amplifier = 2
 export var bank = []
 export var prices = []
 export var gdp = []
@@ -40,6 +42,7 @@ func _ready():
 	variables.get_node("DeviEdit").value = deviation
 	variables.get_node("PoorEdit").value = poor
 	variables.get_node("RichEdit").value = rich
+	variables.get_node("SpecEdit").value = spec
 	variables.get_node("DiveEdit").value = diversity
 	variables.get_node("SlowEdit").pressed = slow
 	variables.get_node("RealismEdit").pressed = realism
@@ -48,29 +51,40 @@ func initialize():
 	for child in grid.get_children():
 		grid.remove_child(child)
 	rng.randomize()
-	grid.columns = sqrt(population)
+	grid.columns = 10#sqrt(population)
 	var curPoor = poor
 	var curRich = rich
+	var curSpec = spec
 	for i in range(population):
+		var rand_spec = rng.randi_range(0, diversity)
 		var curType = "None"
 		var newNeeds = []
 		var newGreeds = []
 		if(curPoor > 0):
 			curType = "Poor"
 			curPoor -= 1
+		elif(curSpec > 0):
+			curType = "Spec"
+			curSpec -= 1
 		elif(curRich > 0):
 			curType = "Rich"
 			curRich -= 1
 		for j in range(0,diversity):
 			newNeeds.append(int(deviation/2)) #* int(need_amplifier)
 			newGreeds.append(rng.randi_range(0, deviation))
+			#newNeeds.append(5)
+			#newGreeds.append(5)
 			if(curType == "Poor"):
 				newGreeds[j] = int(newGreeds[j]/greed_amplifier)
 			elif(curType == "Rich"):
 				newGreeds[j] = int((newGreeds[j]+1)*greed_amplifier)
-			if(realism):
-				if(newNeeds[j] < int(newGreeds[j] * realism_amplifier)):
-					newNeeds[j] = int(newGreeds[j] * realism_amplifier)
+			elif(curType == "Spec"):
+				if j == rand_spec:
+					newGreeds[j] = int((newGreeds[j]+1)*greed_amplifier*spec_amplifier)
+			#TODO: This no longer makes sense, need to overhaul
+#			if(realism):
+#				if(newNeeds[j] < int(newGreeds[j] * realism_amplifier)):
+#					newNeeds[j] = int(newGreeds[j] * realism_amplifier)
 		instance_eed(newNeeds,newGreeds,curType)
 	for j in range(0,diversity):
 		bank.append(0)
@@ -83,7 +97,7 @@ func run_standard_simulation():
 			break
 		else:
 			yield(self,"next_turn")
-		for child in grid.get_children():
+		for child in grid.get_children():#work phase
 			if stopped:
 				break
 			child.work(volatility)
@@ -91,6 +105,13 @@ func run_standard_simulation():
 				child.select(true)
 				yield(self,"next_turn")
 				child.select(false)
+		for child in grid.get_children():#sell phase
+			for j in range(0,diversity):
+				bank[j] += child.sell(j,prices[j])
+		for child in grid.get_children():#buy/survive phase
+			for j in range(0,diversity):
+				bank[j] -= child.buy(j,prices[j],bank[j])
+			child.survive()
 		results(i + 1)
 
 func run_pooled_simulation():
@@ -100,19 +121,19 @@ func run_pooled_simulation():
 			break
 		else:
 			yield(self,"next_turn")
-		for j in range(0,diversity):
+		for j in range(0,diversity):#tax phase
 			for child in grid.get_children():
 				if child.status == STATUS.ALIVE:
 					var tax = child.tax(j,volatility)
 					bank[j] = bank[j] + tax
 			results(i + 1)
-		for j in range(0,diversity):
+		for j in range(0,diversity):#stimulus phase
 			for child in grid.get_children():
 				if bank[j] > child.needs[j] and child.status == STATUS.ALIVE:
 					child.stim(child.needs[j], j)
 					bank[j] = bank[j] - child.needs[j]
 			results(i + 1)
-		for child in grid.get_children():
+		for child in grid.get_children():#survive phase
 			child.survive()
 			if slow:
 				child.select(true)
@@ -162,6 +183,17 @@ func reset():
 	for j in range(0,diversity):
 		bank.append(0)
 	results(0)
+	
+func prices():
+	var gross = 0
+	prices = []
+	for good in gdp:
+		gross += good
+	for good in gdp:
+		if good == 0:
+			prices.append(1)
+		else:
+			prices.append(float(float(gross)/float(good)/gdp.size()))
 
 func _on_Reset_pressed():
 	reset()
@@ -212,6 +244,8 @@ func _on_PoorEdit_value_changed(value):
 func _on_RichEdit_value_changed(value):
 	rich = value
 
+func _on_SpecEdit_value_changed(value):
+	spec = value
 
 func _on_CheckBox_toggled(button_pressed):
 	slow = button_pressed
